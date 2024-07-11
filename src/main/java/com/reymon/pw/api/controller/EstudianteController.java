a246201cdb53651b90779a977dffba5250c411d4
Reymon <rahidalgob@uce.edu.ec>
2024-07-10 19:43:37 -0500

    Taller29 - Level 2.2 of Richardson Maturity Model

--- src/main/java/com/reymon/pw/api/controller/EstudianteController.java
+++ src/main/java/com/reymon/pw/api/controller/EstudianteController.java
@@ -1,9 +1,9 @@
 package com.reymon.pw.api.controller;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,19 +27,25 @@ public class EstudianteController {
 	private IEstudianteService estudianteService;
 
 	@GetMapping(path = "/{id}")
-	public ResponseEntity<Estudiante>  getEstudiante(@PathVariable Integer id) {
-		 this.estudianteService.search(id);
-		return ResponseEntity.status(236).body(this.estudianteService.search(id));
-		
+	public ResponseEntity<Estudiante> getEstudiante(@PathVariable Integer id) {
+		this.estudianteService.search(id);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("hola", "quiubo, estás consultando el resource estudiante");
+		headers.add("msg237", "Resource sent");
+		// return ResponseEntity.status(236).body(this.estudianteService.search(id));
+		return new ResponseEntity<>(this.estudianteService.search(id), headers, 237);
+
 	}
+
 	@GetMapping(path = "/mix/{id}")
 	public Estudiante getByPathVariableAndRequestParam(@PathVariable Integer id, @RequestParam String nombre) {
 		System.out.println(nombre);
 		return this.estudianteService.search(id);
 	}
-	/*searchByGender?genero=M&edad=35 */
+
+	/* searchByGender?genero=M&edad=35 */
 	@GetMapping(path = "/gender")
-	//http://localhost:8080/API/v1.0/Matricula/estudiantes/gender?genero=M
+	// http://localhost:8080/API/v1.0/Matricula/estudiantes/gender?genero=M
 	public List<Estudiante> getEstudiantebyGender(@RequestParam String genero) {
 		return this.estudianteService.searchByGender(genero);
 	}
@@ -49,7 +55,7 @@ public class EstudianteController {
 	public ResponseEntity<Estudiante> save(@RequestBody Estudiante estu) {
 		this.estudianteService.save(estu);
 		return ResponseEntity.status(201).body(estu);
-		
+
 	}
 
 	@PatchMapping(path = "/{id}")
@@ -58,13 +64,11 @@ public class EstudianteController {
 		aux.setApellido(estu.getApellido());
 		this.estudianteService.update(aux);
 		return ResponseEntity.status(239).body(aux);
-		
-		
+
 	}
 
-
 	@PutMapping(path = "/{id}")
-	public ResponseEntity<Estudiante> updateEstudiante(@RequestBody Estudiante estu,@PathVariable Integer id) {
+	public ResponseEntity<Estudiante> updateEstudiante(@RequestBody Estudiante estu, @PathVariable Integer id) {
 		estu.setId(id);
 
 		Estudiante estudiante = estu;
@@ -73,10 +77,9 @@ public class EstudianteController {
 	}
 
 	@DeleteMapping(path = "/{id}") /* SE PUEDE ENVIAR VARIOS PathVariable */
-	public  ResponseEntity<String> deleteEstudiante(@PathVariable Integer id) {
+	public ResponseEntity<String> deleteEstudiante(@PathVariable Integer id) {
 		this.estudianteService.delete(id);
 		return ResponseEntity.status(240).body("Borrada exitosamente");
 	}
-
 
 }
